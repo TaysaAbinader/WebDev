244a610b96105b01d48943a5c70ed97c6f701a94
Taysa Abinader <ferreira.taysa@gmail.com>
2025-11-11 13:15:49 +0200

    Week 4 Wednesday Activity 1.

---
+++ week4/week4-be-activity1/routes/userRouter.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  // patchCar
+} = require("../controllers/userControllers");
+
+// GET /users
+router.get("/", getAllUsers);
+
+// POST /users
+router.post("/", createUser);
+
+// GET /users/:userId
+router.get("/:userId", getUserById);
+
+// PUT /users/:userId
+router.put("/:userId", updateUser);
+
+// DELETE /users/:userId
+router.delete("/:userId", deleteUser);
+
+// Update car using PATCH
+// router.patch('/:blogId', patchBlog)
+
+module.exports = router;
