60a09998dcc0d55e70e87740695ad9bd5e9ddb4d
Taysa Abinader <ferreira.taysa@gmail.com>
2025-11-23 19:07:37 +0200

    Week 6 Monday Activity 2 Part 3

--- week6/part2/custom-hooks-form/src/App.jsx
+++ week6/part2/custom-hooks-form/src/App.jsx
@@ -1,36 +1,55 @@
-import useField from './useField';
+// import useField from './useField';
 
-const AppWithCustomHook = () => {
-  const nameInput = useField('text');
-  const bornInput = useField('date');
-  const heightInput = useField('number');
+// const AppWithCustomHook = () => {
+//   const nameInput = useField('text');
+//   const bornInput = useField('date');
+//   const heightInput = useField('number');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           Name: <input {...nameInput} />
+//         </div>
+//         <br/>
+//         <div>
+//           Birthdate: <input {...bornInput} />
+//         </div>
+//         <br/>
+//         <div>
+//           Height: <input {...heightInput} />
+//         </div>
+//         <button type="submit">Submit</button>
+//       </form>
+
+//       <div>
+//         {nameInput.value} {bornInput.value} {heightInput.value}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AppWithCustomHook;
+
+import useLocalStorage from './useLocalStorage';
+
+const App = () => {
+  const [name, setName] = useLocalStorage('name', '');
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Name: <input {...nameInput} />
-        </div>
-        <br/>
-        <div>
-          Birthdate: <input {...bornInput} />
-        </div>
-        <br/>
-        <div>
-          Height: <input {...heightInput} />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-
-      <div>
-        {nameInput.value} {bornInput.value} {heightInput.value}
-      </div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <p>Your name is stored in localStorage: {name}</p>
     </div>
   );
 };
 
-export default AppWithCustomHook;
+export default App;
